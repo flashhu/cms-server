0742f31565f0f4cff55381c27c1c42e3631262da
flashhu <flashhu@163.com>
2020-08-17 13:19:27 +0800

    refactor：期刊接口知 id, type 得详情部分重写

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -10,23 +10,6 @@ const router = new Router({
     prefix: '/v1/classic'
 })
 
-/**
- * 根据已知flow得到返回数据art
- * @param {number} art_id
- * @param {number} type
- * @param {number} uid
- * @param {number} flow_index 期刊号
- */
-async function flowToArt(art_id, type, uid, flow_index) {
-    const userLikeIt = await Favor.uerLikeIt(art_id, type, uid)
-    const art = await Art.getData(art_id, type)
-    // 序列化：对象 -> json
-    // art 为对象
-    art.setDataValue('index', flow_index)
-    art.setDataValue('likeStatus', userLikeIt)
-    return art
-}
-
 // 获取最新期刊 - 所有用户
 router.get('/latest', new Auth().m, async (ctx, next) => {
     const flow = await Flow.findOne({
@@ -34,7 +17,12 @@ router.get('/latest', new Auth().m, async (ctx, next) => {
             ['index', 'DESC']
         ]
     })
-    ctx.body = await flowToArt(flow.art_id, flow.type, ctx.auth.uid, flow.index)
+
+    const artDetail = await new Art(flow.art_id, flow.type).getDetail(ctx.auth.uid)
+
+    artDetail.art.setDataValue('like_status', artDetail.like_status)
+    artDetail.art.setDataValue('index', flow.index)
+    ctx.body = artDetail.art
 })
 
 // 获取下一期期刊信息 - 所有用户
@@ -47,7 +35,12 @@ router.get('/:index/next', new Auth().m, async (ctx, next) => {
     if(!flow) {
         throw new NotFound()
     }
-    ctx.body = await flowToArt(flow.art_id, flow.type, ctx.auth.uid, flow.index)
+
+    const artDetail = await new Art(flow.art_id, flow.type).getDetail(ctx.auth.uid)
+
+    artDetail.art.setDataValue('like_status', artDetail.like_status)
+    artDetail.art.setDataValue('index', flow.index)
+    ctx.body = artDetail.art
 })
 
 // 获取上一期期刊信息 - 所有用户
@@ -60,7 +53,11 @@ router.get('/:index/prev', new Auth().m, async (ctx, next) => {
     if (!flow) {
         throw new NotFound()
     }
-    ctx.body = await flowToArt(flow.art_id, flow.type, ctx.auth.uid, flow.index)
+    const artDetail = await new Art(flow.art_id, flow.type).getDetail(ctx.auth.uid)
+
+    artDetail.art.setDataValue('like_status', artDetail.like_status)
+    artDetail.art.setDataValue('index', flow.index)
+    ctx.body = artDetail.art
 })
 
 // 获得某期刊信息 - 所有用户
@@ -87,14 +84,12 @@ router.get('/:type/:id/favor', new Auth().m, async (ctx) => {
     const v = await new ClassicValidator().validate(ctx)
     const id = v.get('path.id')
     const type = parseInt(v.get('path.type'))
-    const art = await Art.getData(id, type)
-    if (!art) {
-        throw new NotFound()
-    }
-    const like = await Favor.uerLikeIt(id, type, ctx.auth.uid)
+
+    const artDetail = await new Art(id, type).getDetail(ctx.auth.uid)
+
     ctx.body = {
-        fav_nums: art.fav_nums,
-        like_status: like
+        fav_nums: artDetail.art.fav_nums,
+        like_status: artDetail.like_status
     }
 })
 
